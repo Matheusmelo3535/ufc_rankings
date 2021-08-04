c8ab306f618f34f5a80e28f393f76dd9580da156
Matheusmelo3535 <matheusmelo3535@gmail.com>
2021-08-04 17:11:50 -0300

    n_n_ADD_FIX

--- Controller/LutasController.php
+++ Controller/LutasController.php
@@ -10,15 +10,20 @@ class LutasController extends AppController {
     }
     
     public function add() {
+       if (!empty($this->request->data)) {
+            $this->Luta->create();
+            debug($this->request->data);
+            debug($this->Luta->saveAll($this->request->data));
+            exit();
+            if ($this->Luta->saveAll($this->request->data)) {
+                $this->Flash->set('Deu certo');
+                $this->redirect('/lutas');
+            }
+       }
        $fields = array('Lutador.id', 'Lutador.nome');
        $lutadores =  $this->Luta->Lutador->find('list', compact('fields'));
-       debug($lutadores);
        $this->set('lutadores', $lutadores);
-    }
-    
-    
-        
-    
+    }    
 }
 
 ?>
--- View/Lutas/add.ctp
+++ View/Lutas/add.ctp
@@ -3,11 +3,9 @@
 $form = $this->Form->create('Luta');
 $form .= $this->Form->input('Luta.data_luta');
 $form .= $this->Form->input('Luta.vencedor');
-$form .= $this->Form->input('Luta.Lutador', array('type' => 'select', 'options' => $lutadores));
+$form .= $this->Form->input('Lutador.Lutador', array('multiple' => 'multiple', 'type' => 'select', 'options' => $lutadores));
 $form .= $this->Form->end('Gravar');
 
 echo $this->Html->tag('h1', 'Nova Luta');
 echo $form;
-
-
 ?>
